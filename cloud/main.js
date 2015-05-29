a1f6fa3fbbf06d6a4f995395f8c2542d61c5d85a
oukiar <oukiar@gmail.com>
2015-05-28 22:08:24 -0500

    Print receipts pseudo working

--- cloud/main.js
+++ cloud/main.js
@@ -6,12 +6,18 @@
 require('cloud/app.js');
 
 Parse.Cloud.define("restore", function(request, response) {
-	Parse.Cloud.useMasterKey();
-	  var queryWarehouse = new Parse.Query("Warehouses");
-	  queryWarehouse.equalTo("objectId", request.params.receiptId);
-	  queryWarehouse.first().then(function(results) {
-    results.set("Status", "NotProcessed");
-    results.save();
-	response.success("works");
-			});
-		});
+    Parse.Cloud.useMasterKey();
+    var queryWarehouse = new Parse.Query("Warehouses");
+    queryWarehouse.equalTo("objectId", request.params.receiptId);
+    
+    queryWarehouse.first().then(function(results) 
+    {
+        //obtenemos el status anterior
+        oldstat = results.get("Status");
+        
+        
+        results.set("Status", oldstat.split(":")[1] );
+        results.save();
+        response.success("works");
+    });
+});
