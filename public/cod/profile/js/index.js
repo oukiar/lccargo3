1e1c8794356726fdef98bd7001471c2bb59a40fb
suiGn <jabellae@gmail.com>
2015-08-05 19:59:14 -0700

    estilos y arreglos

--- public/cod/profile/js/index.js
+++ public/cod/profile/js/index.js
@@ -3,15 +3,16 @@
 
 $(document).ready(function() {
 r = function() {
-$('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/sea_con-green-324.png' : 'images/sea_con-green-216.png') : 'images/sea_con-green-108.png');
-$('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/truck-342.png' : 'images/truck-228.png') : 'images/truck-114.png');
-$('.img-3').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/plane-369.png' : 'images/plane-246.png') : 'images/plane-123.png');
-$('.img-4').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/repack-354.png' : 'images/repack-236.png') : 'images/repack-118.png');
-$('.img-5').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/warehouse-354.png' : 'images/warehouse-236.png') : 'images/warehouse-118.png');
-$('.img-6').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/old-354.png' : 'images/old-236.png') : 'images/old-118.png');
-$('.img-7').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/client-348.png' : 'images/client-232.png') : 'images/client-116.png');
-$('.img-8').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/store-348.png' : 'images/store-232.png') : 'images/store-116.png');
-$('.img-9').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/transfers-372.png' : 'images/transfers-248.png') : 'images/transfers-124.png');};
+$('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/store-297.png' : 'images/store-198.png') : 'images/store-99.png');
+$('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/store-297-2.png' : 'images/store-198-2.png') : 'images/store-99-2.png');
+$('.img-3').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/store-297-3.png' : 'images/store-198-3.png') : 'images/store-99-3.png');
+$('.img-4').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/store-297-3.png' : 'images/store-198-3.png') : 'images/store-99-3.png');
+$('.img-5').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/store-297-4.png' : 'images/store-198-4.png') : 'images/store-99-4.png');
+$('.img-6').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/store-297.png' : 'images/store-198.png') : 'images/store-99.png');
+$('.img-7').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/store-297-2.png' : 'images/store-198-2.png') : 'images/store-99-2.png');
+$('.img-8').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/store-297-3.png' : 'images/store-198-3.png') : 'images/store-99-3.png');
+$('.img-9').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/store-297-4.png' : 'images/store-198-4.png') : 'images/store-99-4.png');
+$('.img-10').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/store-297-5.png' : 'images/store-198-5.png') : 'images/store-99-5.png');};
 $(window).resize(r);
 r();
 
